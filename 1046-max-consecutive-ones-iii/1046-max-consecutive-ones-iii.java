07cd810c0eca1aa2b5db3d682a9cd3a783a80f31
Rupak Das <rupak.das.3002@gmail.com>
2025-03-01 11:29:33 +0530

    Time: 4 ms (26.79%), Space: 46.6 MB (54.96%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -4,16 +4,22 @@ class Solution {
         while(r < nums.length) {
             if(nums[r] == 0)
                 zeros++;
+            
+            // Step1: using while loop
+            //------------------------
             // while(zeros > k) {
             //     if(nums[l] == 0)
             //         zeros--;
             //     l++;
             // }
+
+            // Step1: using if condition
             if(zeros > k) {
                 if(nums[l] == 0)
                     zeros--;
                 l++;
             }
+            
             if(zeros <= k) {
                 int len = r - l + 1;
                 maxLen = Math.max(maxLen, len);
