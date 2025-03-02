72c33cf5706e9386ad74884e781dd9707f3fab5f
Rupak Das <rupak.das.3002@gmail.com>
2025-03-02 10:59:39 +0530

    Time: 8 ms (84.88%), Space: 44.7 MB (71.97%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -6,19 +6,24 @@ class Solution {
         int n = s.length();
         int[] hash = new int[26];
 
-        while(r < n) {
+        while (r < n) {
             char c = s.charAt(r);
+            // Update frequency of current character
             hash[c - 65]++; 
             maxFreq = Math.max(maxFreq, hash[c - 65]);
             int len = r - l + 1;
+            // Number of characters to replace
             int count = len - maxFreq; 
-            if(count > k) {
+            
+            // If replacements exceed k, shrink the window
+            if (count > k) { 
                 char ch = s.charAt(l);
                 hash[ch - 65]--;
                 l++;
-            }
+            } 
+            // Update maximum valid window length
             else {
-                maxLen = Math.max(maxLen, len);
+                maxLen = Math.max(maxLen, len); 
             }
             r++;
         }
