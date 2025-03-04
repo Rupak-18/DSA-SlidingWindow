9502d9f890aba6701dfe79ff62a66f76427f6bab
Rupak Das <rupak.das.3002@gmail.com>
2025-03-04 10:14:53 +0530

    Time: 1 ms (100%), Space: 47.5 MB (75.07%) - LeetHub

---
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countSubArray(int[] nums, int k) {
+        if(k < 0)  return 0;
+        int l = 0, r = 0;
+        int count = 0;
+        int sum = 0;
+        int n = nums.length;
+        
+        while(r < n) {
+            sum += nums[r];
+            while(sum > k) {
+                sum -= nums[l];
+                l++;
+            }
+            count += r - l + 1;
+            r++;
+        }
+        return count;
+    }
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return 
+            countSubArray(nums, goal) - countSubArray(nums, goal - 1);
+    }
+}
